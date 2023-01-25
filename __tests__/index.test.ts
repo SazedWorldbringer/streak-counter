39434fdd0fd71c5547bc1054d835390e3bba157d
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-25 16:03:55 +0530

    feat: install jsdom to mock localStorage in tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,6 +1,14 @@
+import { JSDOM } from 'jsdom'
+import { streakCounter } from "../src/index";
+
 describe("streakCounter", () => {
+	let mockLocalStorage: Storage;
+	beforeEach(() => {
+		const mockJSDom = new JSDOM("", {url: "https://localhost"});
+
+		mockLocalStorage = mockJSDom.window.localStorage;
+	})
 	it("should return a streak object with currentCount, startDate and lastLoginDate", () => {
-		const mockLocalStorage = "";
 		const date = new Date();
 		const streak = streakCounter(mockLocalStorage, date);
 
