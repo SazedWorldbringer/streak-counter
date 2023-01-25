ad33f4976d6c278f439a3b6ced8908ced2c1e8e7
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-25 15:50:54 +0530

    feat: write test to see if the initialize streak function returns a streak object which has currentCount, startDate and lastLoginDate properties

---
+++ __tests__/index.test.ts
@@ -0,0 +1,11 @@
+describe("streakCounter", () => {
+	it("should return a streak object with currentCount, startDate and lastLoginDate", () => {
+		const mockLocalStorage = "";
+		const date = new Date();
+		const streak = streakCounter(mockLocalStorage, date);
+
+		expect(streak.hasOwnProperty("currentCount")).toBe(true);
+		expect(streak.hasOwnProperty("startDate")).toBe(true);
+		expect(streak.hasOwnProperty("lastLoginDate")).toBe(true);
+	})
+})
