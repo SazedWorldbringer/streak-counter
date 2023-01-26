42b41feb8a2a2b6bd11c66b948a826fb61fdc549
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-26 23:30:42 +0530

    feat: write a state machine method to decide if it should increment the streak or no

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,28 @@ interface Streak {
 	lastLoginDate: string
 }
 
+export function differenceInDays(dateLeft: Date, dateRight: Date): number {
+	const diffTime = Math.abs(dateLeft.getTime() - dateRight.getTime());
+	const differenceInDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+	return differenceInDays;
+}
+
+function shouldIncrementOrResetStreakCount(
+	currentDate: Date,
+	lastLoginDate: string,
+): 'increment' | undefined {
+	const difference = differenceInDays(currentDate, new Date(lastLoginDate));
+
+	// user logged in a day after the currentDate
+	if (difference === 1) {
+		return 'increment'
+	}
+
+	// user logged in after a day, ultimately breaking the streak
+	return undefined;
+}
+
 // used for storing in localStorage
 const KEY = "streak";
 
@@ -14,7 +36,7 @@ export function streakCounter(storage: Storage, date: Date): Streak {
 	if (streakInLocalStorage) {
 		try {
 			const streak = JSON.parse(streakInLocalStorage);
-			const state = "increment";
+			const state = shouldIncrementOrResetStreakCount(date, streak.lastLoginDate);
 			const SHOULD_INCREMENT = state;
 
 			if (SHOULD_INCREMENT) {
